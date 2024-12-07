85f3cb9ac7d9a7bdfb6ba2b09a5101d9601f649a
PaulaSuarezRodriguez <126203732+PaulaSuarezRodriguez@users.noreply.github.com>
2024-12-07 18:28:05 +0000

    Changes to ActivationFunction class so that sigmoid and ReLu methods can be called in Neural Network class according to string passed as parameter (wether that is sigmoidForward and reluForward for the forward pass or sigmoidBackward and reluBackward for the backward pass)

--- ActivationFunction.py
+++ ActivationFunction.py
@@ -1,17 +1,22 @@
 import numpy as np 
 
 class ActivationFunction:
+    
+    def __init__(self, seed=None):
+        self.seed = seed
+        
     @staticmethod
     def sigmoidForward(x):
-        '''
-        this function does the forward pass of the sigmoid function
-        it takes an input array i just called x 
-        and it returns a tuple with the result (out) of the sigmoid function
-        as well as cache which we use in the backward pass, its just the same as the out 
-        '''
-        out = 1/ (1+ np.exp(-x)) #the sigmoid function
-        cache = out 
-        return out, cache
+         '''
+         this function does the forward pass of the sigmoid function
+         it takes an input array i just called x 
+         and it returns a tuple with the result (out) of the sigmoid function
+         as well as cache which we use in the backward pass, its just the same as the out 
+         '''
+         out = 1/ (1+ np.exp(-x)) #the sigmoid function
+         cache = out 
+         return out, cache
+        
     @staticmethod
     def sigmoidBackward(dout, cache):
         '''
@@ -24,7 +29,7 @@ class ActivationFunction:
         sig = cache
         dx = dout * sig * (1 - sig) #the derivative of the sigmoid function multiplied by the upstream gradient to get the proper flow of gradients
         return dx
-
+        
     @staticmethod   
     def reluForward(x):
         '''
@@ -36,6 +41,7 @@ class ActivationFunction:
         out = np.maximum(0,x)
         cache = x 
         return out, cache
+    
     @staticmethod
     def reluBackward(dout, cache):
         '''
@@ -47,3 +53,38 @@ class ActivationFunction:
         x = cache 
         dx = dout * (x > 0) #derivative is 1 when x >0 otherwise it is 0
         return dx
+
+#activation function general which is the one always called which then calls the appropriate one by using if statements that compare the name of the string provided when running to the if statement for that string that says if string relu then call relu out of all activation functions
+
+
+    @staticmethod
+    def whichActivationFunctionForwardPass(activation_function_string, output_vector):
+        if activation_function_string == "sigmoidForward":
+            return ActivationFunction.sigmoidForward(output_vector)
+        
+        elif activation_function_string == "reluForward":
+            return ActivationFunction.reluForward(output_vector)
+
+            
+            
+    @staticmethod            
+    def whichActivationFunctionBackwardPass(activation_function_string, output_vector):
+                
+        if activation_function_string == "sigmoidBackward":
+            return ActivationFunction.sigmoidBackward(output_vector)
+                
+        elif activation_function_string == "reluBackward":
+            return ActivationFunction.reluBackward(output_vector)
+       
+                
+                
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
