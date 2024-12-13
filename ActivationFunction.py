3dd5168d5d9c0a8b54630a0abed50765794222e5
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2024-12-13 10:05:20 +0000

    Updated Activation function
    fixed some errors and bugs regarding the datatypes being passed in and returned

--- ActivationFunction.py
+++ ActivationFunction.py
@@ -13,20 +13,29 @@ class ActivationFunction:
          and it returns a tuple with the result (out) of the sigmoid function
          as well as cache which we use in the backward pass, its just the same as the out 
          '''
-         out = 1/ (1+ np.exp(-x)) #the sigmoid function
+         out = 1 / (1 + np.exp(-x)) #the sigmoid function
          cache = out 
          return out, cache
         
     @staticmethod
     def sigmoidBackward(dout, cache):
         '''
-        does the backward pass of the simoid function
+        does the backward pass of the sigmoid function
         i used d to show that its the derivative 
         so dx is the gradient of the loss with resepct to x (the input array)
         dout is the upstream gradient
         sig is just the sigmoid function hence why it equals cache
         '''
-        sig = cache
+        # Ensure inputs are numpy arrays
+        dout = np.array(dout)
+        
+        # Handle case where cache might be None
+        if cache is None:
+            raise ValueError("Cache cannot be None in sigmoidBackward")
+        
+        sig = np.array(cache)
+        
+        # Compute gradient
         dx = dout * sig * (1 - sig) #the derivative of the sigmoid function multiplied by the upstream gradient to get the proper flow of gradients
         return dx
         
@@ -50,12 +59,17 @@ class ActivationFunction:
         dx is the gradient of the loss in respect to the input (being the array x)
         dout is the upstream gradient 
         '''
-        x = cache 
+        # Ensure inputs are numpy arrays
+        dout = np.array(dout)
+        
+        # Handle case where cache might be None
+        if cache is None:
+            raise ValueError("Cache cannot be None in reluBackward")
+        
+        x = np.array(cache)
+        
         dx = dout * (x > 0) #derivative is 1 when x >0 otherwise it is 0
         return dx
-
-#activation function general which is the one always called which then calls the appropriate one by using if statements that compare the name of the string provided when running to the if statement for that string that says if string relu then call relu out of all activation functions
-
 
     @staticmethod
     def whichActivationFunctionForwardPass(activation_function_string, output_vector):
@@ -65,26 +79,15 @@ class ActivationFunction:
         elif activation_function_string == "reluForward":
             return ActivationFunction.reluForward(output_vector)
 
-            
-            
-    @staticmethod            
-    def whichActivationFunctionBackwardPass(activation_function_string, output_vector):
-                
+    @staticmethod
+    def whichActivationFunctionBackwardPass(activation_function_string, output_vector, cache=None):
         if activation_function_string == "sigmoidBackward":
-            return ActivationFunction.sigmoidBackward(output_vector)
-                
+            if cache is None:
+                raise ValueError("Cache is None for sigmoidBackward")
+            return ActivationFunction.sigmoidBackward(output_vector, cache)
         elif activation_function_string == "reluBackward":
-            return ActivationFunction.reluBackward(output_vector)
-       
-                
-                
-            
-    
-    
-    
-    
-    
-    
-    
-    
-    
+            if cache is None:
+                raise ValueError("Cache is None for reluBackward")
+            return ActivationFunction.reluBackward(output_vector, cache)
+        else:
+            raise ValueError(f"Unsupported activation function: {activation_function_string}")
