b4f80fc01a9d1d431b8d7edf5f2daa6d007996fa
PaulaSuarezRodriguez <126203732+PaulaSuarezRodriguez@users.noreply.github.com>
2024-12-27 09:34:16 +0000

    Corrected logic regarding application of optimizers.

--- CIFAR10Runner.py
+++ CIFAR10Runner.py
@@ -2,6 +2,8 @@ import os
 # disables oneDNN optimisations
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
+import random 
+from random import randint
 import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,13 +11,11 @@ from NeuralNetwork import NeuralNetwork
 from Optimisers import AdamOptimiser, SGDMomentumOptimiser, SGDOptimiser
 
 class CIFAR10Runner:
-    def __init__(self, model, epochs, batch_size, patience, tolerance):
+    def __init__(self, model, epochs, batch_size): # ADD TO CHALLENGES FACED. epoch, batch_size and to some extent, learning_rate (although not really) are also hyperparameters in NN, but are implemented alsewhere because of XYZ ––> "Report the parameters used (update rule, learning rate, decay, epochs, batch size)", Section E, Spec. 
         # Neural Network parameters
         self.model = model
         self.epochs = epochs
         self.batch_size = batch_size
-        self.patience = patience
-        self.tolerance = tolerance
 
     def load_data(self):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
@@ -44,19 +44,21 @@ class CIFAR10Runner:
         print("Loading CIFAR-10 data...")
         x_train, y_train, x_val, y_val, x_test, y_test = self.load_data()
         
-        self.model.train(x_train, y_train, x_val, y_val, self.epochs, self.batch_size, self.patience, self.tolerance)
+        self.model.train(x_train, y_train, x_val, y_val, self.epochs, self.batch_size)
 
         print("Final Evaluation on Test Set...")
         test_accuracy = self.model.run(x_test, y_test)
         print(f"Test Accuracy: {test_accuracy * 100:.2f}%")
-
+             
         self.model.plot_loss()
+   
+    
 
 if __name__ == "__main__":
 
     adamOptimiser = AdamOptimiser(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, decay=0.01)
     sgdMomentumOptimiser = SGDMomentumOptimiser(learning_rate=0.001, momentum=0.9, decay=0.01)
-    sgdOptimiser = SGDOptimiser(learning_rate=0.001, decay=0.01)
+    sgdOptimiser = SGDOptimiser(learning_rate=0.001, decay=0.01) 
 
     model = NeuralNetwork(
         activationFunction = "relu",
@@ -64,15 +66,14 @@ if __name__ == "__main__":
         output_size = 10,
         hidden_units = [1024, 512, 256],
         dropout_rate = 0.2,  # 0.4,
-        optimisers = [adamOptimiser, sgdMomentumOptimiser, sgdOptimiser],
+        optimisers = [adamOptimiser], #, sgdMomentumOptimiser, sgdOptimiser],
         l2_lambda = 0.0
     )
 
     runner = CIFAR10Runner(
         model = model,
-        epochs = 100,
+        epochs = 30,
         batch_size = 128,
-        patience = 5,
-        tolerance = 0.01
     ) 
+        
     runner.run()
