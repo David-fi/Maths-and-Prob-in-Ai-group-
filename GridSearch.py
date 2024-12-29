1324746a3e4bedc597da389ac5b6a5983dcb4ebd
PaulaSuarezRodriguez <126203732+PaulaSuarezRodriguez@users.noreply.github.com>
2024-12-29 03:24:24 +0000

    param_grid 1 and 2 to be edited once param grid 3 returns since param grid 3 is actually the second (that based on param_grid1)

--- GridSearch.py
+++ GridSearch.py
@@ -113,23 +113,13 @@ if __name__ == "__main__":
     
     param_grid1 = {
         'activationFunction': ['relu', 'tanh', 'sigmoid'],
-        'hidden_units': [randint(10, 2000) for _ in range(randint(3, 10))],
+        'hidden_units': [[randint(128, 512), randint(128, 512), randint(128, 512)] for _ in range(3)],
         'dropout_rate': [0.1, 0.2, 0.5],
         'epoch': [10, 15, 30, 40],
         'batch_size': [16, 128],
         'l2_lambda': [1e-6, 1e-4]
     }
-    
-    param_grid2 = {
-        'activationFunction': ['relu', 'tanh', 'sigmoid'],
-        'hidden_units': [randint(128, 256) for _ in range((3))],
-        'dropout_rate': [0.1, 0.2, 0.5],
-        'epoch': [30],
-        'batch_size': [128],
-        'l2_lambda': [1e-6, 1e-4]
-    
-    }
-    
+        
     param_grid3 = {
         'activationFunction': ['relu', 'tanh', 'sigmoid'],
         'hidden_units': [[randint(128, 512), randint(128, 512), randint(128, 512)] for _ in range(3)],  # Three hidden layers
@@ -139,5 +129,15 @@ if __name__ == "__main__":
         'l2_lambda': [1e-6, 1e-5, 1e-4]
     }
 
+    param_grid2 = {
+        'activationFunction': ['relu', 'tanh', 'sigmoid'],
+        'hidden_units':[[randint(128, 512), randint(128, 512), randint(128, 512)] for _ in range(3)],
+        'dropout_rate': [0.1, 0.2, 0.5],
+        'epoch': [30],
+        'batch_size': [128],
+        'l2_lambda': [1e-6, 1e-4]
+    
+    }
+    
     #GridSearch(param_grid_for_quick_testing)
     GridSearch(param_grid3)
